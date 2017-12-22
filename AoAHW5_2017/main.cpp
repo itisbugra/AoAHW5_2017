cde5e2b18deca2210b4d9090cab563d04ee06b94
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-22 16:36:46 +0300

    Initial Commit

---
+++ AoAHW5_2017/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AoAHW5_2017
+//
+//  Created by Buğra Ekuklu on 22.12.2017.
+//  Copyright © 2017 BuyBuddy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
